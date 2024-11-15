31de3e401aafd43463c9fbd3bcace5a69d5fd3c8
Giftzwerg02 <chessplayer1@gmx.at>
2024-11-15 01:15:23 +0100

    ctf/pwn template

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
       assert builtins.isPath path;
         lib.last (
           builtins.split
-          "flake-templates/"
+          "flake-templates/templates/"
           (toString path)
         );
 
