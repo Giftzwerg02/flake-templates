26afcb200669b6feda717a13314652904ab39ebd
Giftzwerg02 <chessplayer1@gmx.at>
2024-11-20 21:58:09 +0100

    lang/c

---
+++ templates/lang/c/default/flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            gcc
+            gnumake
+            gdb
+            linux-manual 
+            man-pages 
+            man-pages-posix
+          ];
+        };
+      }
+    );
+}
+
